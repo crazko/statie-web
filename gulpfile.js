cd3d0ab0dde5a1a95e840fff6b8a87beefad52cd
Roman Vesely <crazko@gmail.com>
2019-01-01 14:35:01 +0100

    Update layout structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,26 @@
-const browserSync = require('browser-sync').create();
-const del = require('del');
-const gulp = require('gulp');
-const run = require('gulp-run');
+const browserSync = require("browser-sync").create();
+const del = require("del");
+const gulp = require("gulp");
+const run = require("gulp-run");
 
 const paths = {
-  dist: 'output',
+  dist: "output"
 };
 
 const clean = () => del([paths.dist]);
-const generate = () => run('vendor/bin/statie generate source').exec();
+const generate = () => run("vendor/bin/statie generate source").exec();
 const reload = done => {
   browserSync.reload();
   done();
-}
+};
 const watch = () => {
   browserSync.init({
     server: paths.dist
   });
 
-  gulp.watch(['*.yml', 'source'], gulp.series(generate, reload));
-}
+  gulp.watch(["*.yml", "source"], gulp.series(generate, reload));
+};
 
-gulp.task('default', gulp.series(clean, generate, watch));
+gulp.task("default", gulp.series(clean, generate, watch));
+gulp.task(generate);
 gulp.task(clean);
